dd6137c6473befbc8d92f33cca89b97fc596460a
lekoal <coreglus@gmail.com>
2021-09-15 12:58:02 +0300

    Tasks completed

--- src/ru/geekbrains/java_algoritms_homework_12/Main.java
+++ src/ru/geekbrains/java_algoritms_homework_12/Main.java
@@ -2,12 +2,16 @@ package ru.geekbrains.java_algoritms_homework_12;
 
 import java.util.Arrays;
 
-public class Main{
+public class Main {
 
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
 
     public static void main(String[] args) {
+        System.out.print("Single thread method execution time: ");
+        method1();
+        System.out.println();
+        System.out.print("Double thread method execution time: ");
         method2();
     }
 
@@ -29,7 +33,28 @@ public class Main{
         long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, arr1, 0, HALF);
         System.arraycopy(arr, HALF, arr2, 0, HALF);
+        Thread t1 = new Thread(arrayCalculate(arr1));
+        Thread t2 = new Thread(arrayCalculate(arr2));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(arr1, 0, arr, 0, HALF);
+        System.arraycopy(arr2, 0, arr, HALF, HALF);
 
+        System.out.println(System.currentTimeMillis() - a);
 
+    }
+
+    static Runnable arrayCalculate(float[] arr) {
+        return () -> {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2F + (float) i / 5) * Math.cos(0.2F + (float) i / 5) * Math.cos(0.4F + (float) i / 2));
+            }
+        };
     }
 }
